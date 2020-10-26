0230f03808921987cf6ccb175f998f8893e13ed6
MD Tareq Mahmud <tareqmahmud9@gmail.com>
2020-10-27 00:00:27 +0600

    Add profile model

---
+++ models/Profile.js
@@ -0,0 +1,53 @@
+const {Schema, model} = require('mongoose');
+
+const ProfileSchema = new Schema({
+    user: {
+        type: Schema.Types.objectId,
+        ref: 'User',
+        required: true
+    },
+
+    title: {
+        type: String,
+        trim: true,
+        maxlength: 100
+    },
+
+    bio: {
+        type: String,
+        trim: true,
+        maxlength: 500
+    },
+
+    profilePic: {
+        type: String,
+    },
+
+    links: {
+        website: String,
+        facebook: String,
+        twitter: String,
+        github: String
+    },
+
+    posts: [
+        {
+            type: Schema.Types.objectId,
+            ref: 'Post'
+
+        }
+    ],
+
+    bookmarks: [
+        {
+            type: Schema.Types.objectId,
+            ref: 'Post'
+        }
+    ]
+
+
+}, {timestamps: true});
+
+const Profile = model('Profile', ProfileSchema);
+
+module.exports = Profile;
